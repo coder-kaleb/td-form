fcd88eec7e045e59afc100d80de28e93e674e7be
kaleb <121309809+starbouns@users.noreply.github.com>
2023-10-13 19:43:19 +0300

    bug fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,4 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 const Home = () => {
-  const router = useRouter();
-  router.push("/register");
   return (
     <h1 className="mt-10 text-center text-4xl font-bold">
       Home page coming soon
