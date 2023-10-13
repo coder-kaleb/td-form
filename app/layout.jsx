8c202442c1d62d207fcf3a329fc109353442dbc0
kaleb <121309809+starbouns@users.noreply.github.com>
2023-10-13 19:59:34 +0300

    theme-color added

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,12 +4,13 @@ import { Navbar } from "@/components";
 
 export const metadata = {
   title: "Student Form",
+  themeColor: "bg-primaryBlack",
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className=" bg-white">
         <main className="px-5 lg:px-32 ">
           <Navbar />
           {children}
